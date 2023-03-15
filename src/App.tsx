23d2786eeb00491a5b4833e4b170325059619c70
tmdev <am.chirag.pm@gmail.com>
2023-03-15 14:37:25 +0530

    adding default boiler plate

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="d-flex justify-content-center p-2">Matrimony Site</h1>
+    </div>
+  );
+}
+
+export default App;
