6c79a8d3fbfbc9e4151b558d44f6f47668d09738
ShivaniMakvana <shivani105027@gmail.com>
2023-03-16 14:31:37 +0530

    login

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,54 +1,64 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+
 
 function Login() {
   return (
-    <Container className="d-flex justify-content-center">
-      <Row className="container-xl ">
-        <Col className="container-xl" xs={6}>
-          <div className="container-xl">
-            <img
-              className="container-xxl"
-              src={require("../../images/login.png")}
-              alt=""
-            />
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col">
+        <div className="card card-registration">
+          <div className="row g-0">
+            <div className="col-xl-6 d-none d-xl-block">
+              <img
+                src={require("../../images/login.jpg")}
+                alt="Sample photo"
+                className="img-fluid"
+                // style={{border-top-left-radius: .25rem; border-bottom-left-radius: .25rem;}}
+              />
+            </div>
+            <div className="col-xl-6">
+              <div className="card-body p-md-5 text-black">
+                <h3 className="mb-2 text-center">Let’s get started now!</h3>
+                <h5 className="mb-5 text-center">
+                  Login and find your life partner
+                </h5>
+
+                <Form>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check
+                      type="checkbox"
+                      label="Remember me on this device"
+                    />
+                  </Form.Group>
+                  <Button
+                    className="mb-5 text-center"
+                    variant="primary"
+                    type="submit"
+                  >
+                    Submit
+                  </Button>
+                  <br />
+                  <Form.Text className="text-muted">
+                    Forgot Password ?
+                  </Form.Text>
+                  <p className="formFieldLink">
+                    New to PerfectMatch.com ? Register Free
+                  </p>
+                </Form>
+              </div>
+            </div>
           </div>
-        </Col>
-        <Col className="container-xl" xs={6}>
-          <div>
-            <h2 className="text-center">Let’s get started now!</h2>
-            <h5 className="text-center">Login and find your life partner</h5>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check
-                  type="checkbox"
-                  label="Remember me on this device"
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-              <br />
-              <Form.Text className="text-muted">Forgot Password ?</Form.Text>
-              <p className="formFieldLink">
-                New to PerfectMatch.com ? Register Free
-              </p>
-            </Form>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+        </div>
+      </div>
+    </div>
   );
 }
 
