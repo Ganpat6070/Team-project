8b69ff1412004d34a0269cb7b3bf93339638bd56
ShivaniMakvana <shivani105027@gmail.com>
2023-03-16 14:07:23 +0530

    login

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -7,21 +7,24 @@ import Col from "react-bootstrap/Col";
 function Login() {
   return (
     <Container className="d-flex justify-content-center">
-      <Row>
-        <Col xs={6}>
-          <div>
-            <img src={require("../../images/login.png")} alt="" />
+      <Row className="container-xl ">
+        <Col className="container-xl" xs={6}>
+          <div className="container-xl">
+            <img
+              className="container-xxl"
+              src={require("../../images/login.png")}
+              alt=""
+            />
           </div>
         </Col>
-        <Col xs={6}>
+        <Col className="container-xl" xs={6}>
           <div>
+            <h2 className="text-center">Let’s get started now!</h2>
+            <h5 className="text-center">Login and find your life partner</h5>
             <Form>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
@@ -36,6 +39,8 @@ function Login() {
               <Button variant="primary" type="submit">
                 Submit
               </Button>
+              <br />
+              <Form.Text className="text-muted">Forgot Password ?</Form.Text>
               <p className="formFieldLink">
                 New to PerfectMatch.com ? Register Free
               </p>
